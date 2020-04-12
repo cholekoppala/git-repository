0713a169789de8e263cd0a895c163881c3e5ef33
subad <subad@192.168.100.11>
2020-04-13 01:50:52 +0530

    properties application

--- spring-cloud-config-server/src/main/java/com/java/cloud/app/demo/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/java/cloud/app/demo/SpringCloudConfigServerApplication.java
@@ -1,11 +1,15 @@
 package com.java.cloud.app.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.java")
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
